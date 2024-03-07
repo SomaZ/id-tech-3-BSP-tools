2b46127c56e4faefe448a046d96d236cb852177f
SomaZ <17459161+SomaZ@users.noreply.github.com>
2024-03-07 07:52:19 +0100

    [nomip] Correctly skip nomip shaders
    Also added some comments

--- force_ext_lm_nomip.py
+++ force_ext_lm_nomip.py
@@ -20,6 +20,8 @@ def main():
 	if len(split) < 2:
 		print("Could not find base path in: " , fixed_file_path)
 		print("BSP file must be in the 'maps' folder in the base path")
+		print("Press Key to quit")
+		input()
 		return
 	base_path = split[0] + '/'
 	print("Guessed base path:" + base_path)
@@ -41,22 +43,25 @@ def main():
 	for lump in bsp.lumps:
 		print(lump, "Number of Elements:", len(bsp.lumps[lump]))
 
+	# Guess the correct shader path
 	shader_path = "shaders/"
 	if not os.path.exists(base_path + shader_path):
 		shader_path = "scripts/"
 
+	# Make sure shader path exists
 	if not os.path.exists(base_path + shader_path):
 		os.makedirs(base_path + shader_path)
 		print("Created new folder for the shader file:", base_path + shader_path)
 
+	# Find external lightmaps
 	reg_path = bsp.map_name + "/lm_[0-9]{4}"
 	for format in LIGHTMAP_FORMATS:
 		external_lm_files = vfs.search(reg_path + format)
 		if external_lm_files:
 			break
 	if len(external_lm_files) == 0:
-		print("No external shaders found")
-		print("Aborting")
+		print("No external lightmaps found")
+		print("Press Key to quit")
 		input()
 		return
 
@@ -64,30 +69,41 @@ def main():
 	print("Shader path:", shader_path)
 
 	nomip_shader_names = [
-		"{}/force_nomip_{}".format(map_name, i) for i in range(
+		"{}/force_nomip_{}".format(map_name, i).lower() for i in range(
 			ceil(len(external_lm_files) / 8)
 			)
 		]
 	
+	# Read q3map2 shader and remove previous nomip shaders
 	new_shader_file_lines = []
 	shader_file = vfs.get("{}q3map2_{}.shader".format(shader_path, map_name))
 	if shader_file is not None:
 		lines = shader_file.decode(encoding="latin-1").splitlines()
 		open_shader = 0
 		for line in lines:
-			if line.strip() in nomip_shader_names:
+			# shader name
+			if line.strip().lower() in nomip_shader_names:
 				open_shader = 1
 				continue
+			# shader opeing or stage opening
 			if open_shader > 0 and line.strip().startswith("{"):
 				open_shader += 1
 				continue
+			# shader closing or stage closing
 			if open_shader > 0 and line.strip().startswith("}"):
 				open_shader -= 1
 				continue
-			if open_shader > 0:
+			# shader body
+			if open_shader > 1:
 				continue
+			# empty line after shader
+			if open_shader == 1:
+				open_shader -= 1
+				continue
+			# These are not the lines we are looking for
 			new_shader_file_lines.append(line)
 
+	# Add new nomip shaders
 	for id, shader_name in enumerate(nomip_shader_names):
 		first_external_lightmap_index = id * 8
 		last_external_lightmap_index = min(len(external_lm_files), first_external_lightmap_index + 8)
@@ -102,11 +118,12 @@ def main():
 		new_shader_file_lines.append("}")
 		new_shader_file_lines.append("")
 
+	# Add shaders to the bsp and create new surfaces
 	new_surface_lump = []
 	for shader_name in nomip_shader_names:
-		if len(shader_name) > 62:
+		if len(shader_name) > 63:
 			print("Shader name would be bigger than 63 chars. Shortening the bsp file name might help.")
-			print("Aborting")
+			print("Press Key to quit")
 			input()
 			return
 		new_shader = bsp.lump_info["shaders"]()
@@ -117,15 +134,16 @@ def main():
 		new_surface.texture = bsp.lumps["shaders"].index(new_shader)
 		new_surface_lump.append(new_surface)
 	
+	# Fix all face references
 	for leaf in bsp.lumps["leaffaces"]:
 		leaf.face += len(new_surface_lump)
 	for model in bsp.lumps["models"]:
 		model.face += len(new_surface_lump)
-
 	if hasattr(bsp.lumps["brushsides"][0], "face"):
 		for side in bsp.lumps["brushsides"]:
 			side.face += len(new_surface_lump)
 
+	# Add the new surfaces to the head of the surfaces lump
 	bsp.lumps["surfaces"] = new_surface_lump + bsp.lumps["surfaces"]
 
 	new_bsp_bytes = bsp.to_bytes()
@@ -137,7 +155,7 @@ def main():
 			
 	except PermissionError:
 		print("Doesn't have permission to write to ", new_file_name)
-		print("Aborting")
+		print("Press Key to quit")
 		input()
 		return
 
@@ -150,11 +168,12 @@ def main():
 			
 	except PermissionError:
 		print("Doesn't have permission to write to ", new_file_name)
-		print("Aborting")
+		print("Press Key to quit")
 		input()
 		return
 
 	print("Finished successfully")
+	print("Press Key to quit")
 	input()
 
 if __name__ == "__main__":
